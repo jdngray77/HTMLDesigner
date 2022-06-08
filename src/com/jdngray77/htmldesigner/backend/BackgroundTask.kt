a19e2c6ff8d75a3f0dd81a8bbe28685d1739371a
Dylan <dylanbrand@gmail.com>
2022-06-08 14:06:31 +0100

    Basic thread pool for background tasks
    - A task can be submitted to the thread pool for execution.
    - Upon application close, all remaining tasks are executed in an orderly manner and the thread pool is then terminated.
    - Evolutionary improvements are to be expected and will be implemented accordingly when necessary.

--- src/com/jdngray77/htmldesigner/backend/BackgroundTask.kt
+++ src/com/jdngray77/htmldesigner/backend/BackgroundTask.kt
@@ -1,25 +1,52 @@
 package com.jdngray77.htmldesigner.backend
 
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
 /**
  * Runs tasks in the background
  */
-object BackgroundTask {
+object BackgroundTask : Subscriber {
+
+    private val threadPool = Executors.newCachedThreadPool()
+
+    init {
+        EventNotifier.subscribe(this, EventType.USER_EXIT)
+    }
+
 
     fun runOnUIThread(runnable: Runnable) {
         TODO()
     }
 
+    
+    /**
+     * Submits a [runnable] task to the for [threadPool] for execution.
+     */
     fun submit(runnable: Runnable) {
-        TODO()
+        threadPool.submit(runnable)
     }
+
 
     /**
-     * - Disallow new tasks to be submitted,
-     * - wait here for all threads to close?
-     * - close the thread pool
+     * Shutdowns the application's [threadPool].
+     *
+     * Executes the [threadPool]'s remaining unfinished tasks and shuts it down on completion.
      */
-    fun shutdown() {
+    private fun shutdown() {
+        threadPool.shutdown()
 
+        try {
+            if (!threadPool.awaitTermination(10L, TimeUnit.SECONDS))
+                threadPool.shutdownNow() // terminates remaining tasks after 10 seconds
+        } catch (e: InterruptedException) {
+            return
+        }
+
+        threadPool.shutdownNow() // shutdown thread pool upon task(s) completion
     }
 
+    override fun notify(e: EventType) {
+        shutdown()
+    }
 }
