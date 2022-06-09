484b01f751eb23b3b2ab1b03e459116da15be881
Jordan T. Gray <business@shinkson47.in>
2022-06-09 21:48:33 +0100

    Tag hierarchy can delete one or many tags.

--- src/com/jdngray77/htmldesigner/frontend/docks/TagHierarchy.kt
+++ src/com/jdngray77/htmldesigner/frontend/docks/TagHierarchy.kt
@@ -4,11 +4,14 @@ import com.jdngray77.htmldesigner.StoringTreeItem
 import com.jdngray77.htmldesigner.backend.EventNotifier
 import com.jdngray77.htmldesigner.backend.EventType
 import com.jdngray77.htmldesigner.backend.Subscriber
-import com.jdngray77.htmldesigner.frontend.Editor.Companion.EDITOR
 import com.jdngray77.htmldesigner.frontend.Editor.Companion.mvc
 import com.jdngray77.htmldesigner.frontend.docks.dockutils.HierarchyDock
+import javafx.scene.control.Button
+import javafx.scene.control.SelectionMode
+import javafx.scene.layout.HBox
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+
 
 /**
  * A dock which can display the hierarchy of a single
@@ -18,16 +21,40 @@ import org.jsoup.nodes.Element
  */
 class TagHierarchy : HierarchyDock<Element>({it!!.tagName()}), Subscriber {
 
+    private val buttons = HBox()
+
+    /**
+     * Reference to the document whose tags we are displying.
+     */
+    private lateinit var document: Document
 
     init {
-        center = tree
-        tree.isEditable = false
+        tree.selectionModel.selectionMode = SelectionMode.MULTIPLE
+
         EventNotifier.subscribe(this, EventType.EDITOR_DOCUMENT_SWITCH, EventType.EDITOR_DOCUMENT_EDITED)
 
         tree.setOnMouseClicked {
-            tree.selectionModel.selectedItem?.apply {
-                mvc().MainView.textEditor_Open((this as StoringTreeItem<Element>).data.toString())
+            selectedTags().apply {
+                mvc().MainView.textEditor_Open(
+                    if (size != 1)
+                        ""
+                    else
+                        first().toString()
+                )
             }
+        }
+
+        top = buttons
+        buttons.children.apply {
+            add(Button("Delete").apply {
+                setOnAction {
+                    mvc().apply {
+                        deleteTag(*selectedTags().toTypedArray())
+
+                    }
+                    showDocument(document)
+                }
+            })
         }
     }
 
@@ -37,7 +64,8 @@ class TagHierarchy : HierarchyDock<Element>({it!!.tagName()}), Subscriber {
      * Will be overwritten on the next document edit or editor switch.
      */
     fun showDocument(doc: Document) {
-        setRoot(doc).also {
+        document = doc
+        setRoot(doc.body()).also {
             it.value = doc.title()
         }
     }
@@ -47,6 +75,15 @@ class TagHierarchy : HierarchyDock<Element>({it!!.tagName()}), Subscriber {
         if (e == EventType.EDITOR_DOCUMENT_SWITCH || e == EventType.EDITOR_DOCUMENT_EDITED)
             showDocument(mvc().MainView.currentDocument())
     }
+
+    /**
+     * If an element **inside** the root is selected within the [tree], it is returned.
+     */
+    fun selectedTags() =
+        tree.selectionModel.selectedItems
+            .filterNot { it == tree.root }
+            .map { (it as StoringTreeItem<Element>).data }
+            .filterNotNull()
 
     override fun getChildrenFor(el: Element): Iterable<Element> = el.children()
 
