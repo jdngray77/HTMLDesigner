3b36c02bdbf44a2f0530226977829b47ba80def9
Jordan T. Gray <business@shinkson47.in>
2022-06-10 21:35:10 +0100

    Created better distinction for model / document editor.

--- src/com/jdngray77/htmldesigner/MVC.kt
+++ src/com/jdngray77/htmldesigner/MVC.kt
@@ -1,29 +1,42 @@
 package com.jdngray77.htmldesigner
 
-import com.jdngray77.htmldesigner.backend.EventNotifier
-import com.jdngray77.htmldesigner.backend.EventType
-import com.jdngray77.htmldesigner.backend.Subscriber
+import com.jdngray77.htmldesigner.backend.*
 import com.jdngray77.htmldesigner.backend.data.Project
-import com.jdngray77.htmldesigner.backend.userConfirm
+import com.jdngray77.htmldesigner.frontend.DocumentEditor
 import com.jdngray77.htmldesigner.frontend.MainViewController
+import javafx.scene.control.ButtonType
+import javafx.scene.control.Tab
+import javafx.scene.layout.BorderPane
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.io.File
 
 
 /**
- * Model View Controller.
+ * # Model View Controller.
  *
- * Editor requests change > MVC > Change the data
- * Data is updated or loaded > MVC > Editor is updated to display it.
+ * Central location for reaching :
+ *  - GUI
+ *  - Project
+ *  - Current [DocumentEditor].
  *
- * Communication between the backend and from end must
- * pass through here.
+ *  The entire IDE uses the data and API here
+ *  to access and mutate the model.
  */
 class MVC (
 
+    /**
+     * The project the IDE is working on.
+     *
+     * Use to access files on the disk.
+     */
     var Project : Project,
 
+    /**
+     * The IDE's main FXML GUI controller.
+     *
+     * Use to access the front-end.
+     */
     var MainView : MainViewController
 
 ) : Subscriber {
@@ -34,24 +47,144 @@ class MVC (
 
     override fun notify(e: EventType) {
         if (e == EventType.EDITOR_LOADED)
-            MainView.openDocument(Project.loadDocument(Project.documents().first()))
-
-
-//        when (e) {
-//            EventType.EDITOR_OPEN_DOCUMENT_CHANGED ->
-//                MainView.updateDisplay()
-//        }
+            openDocument(Project.loadDocument(Project.documents().first()))
     }
 
-    fun document() = MainView.currentDocument()
 
-    fun newStylesheet(name: String) = Project.createStylesheet(name)
+    //░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+    //region                                                        Editors
+    //░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
+    /**
+     * A list of all open editors.
+     */
+    private val openEditors =
+        ArrayList<DocumentEditor>()
+
+    /**
+     * Returns the document of the current editor
+     */
+    fun currentDocument() =
+        currentEditor()!!.document
+
+    /**
+     * It returns the current editor
+     */
+    fun currentEditor() =
+        this.findEditorFor(MainView.dockEditors.selectionModel.selectedItem)!!
+
+
+    /**
+     * Finds the editor for the given document.
+     *
+     * @param document The document to find an editor for.
+     */
+    fun findEditorFor(document: Document)  =
+        openEditors.find { it.document == document }
+
+    /**
+     * Finds the editor for the given tab.
+     *
+     * @param tab The tab to find the editor for.
+     */
+    fun findEditorFor(tab: Tab)  =
+        openEditors.find { it.tab == tab }
+
+    /**
+     * Creates and opens a new document editor for the
+     * given [document]
+     *
+     * Create a new document editor, set the document,
+     * add the editor to the list of open editors, and switch to the
+     * new editor
+     *
+     * @param document Document - The document to open
+     */
+    fun openDocument(document: Document) {
+        loadFXMLComponent<BorderPane>("DocumentEditor.fxml").apply {
+            Tab(document.title(), first).let {
+                MainView.dockEditors.tabs.add(it)
+
+                first.prefWidthProperty().bind(MainView.dockEditors.widthProperty())
+                first.prefHeightProperty().bind(MainView.dockEditors.heightProperty())
+
+                (second as DocumentEditor).apply {
+                    setDocument(document, it)
+                    openEditors.add(this)
+                    switchToEditor(this)
+
+                    it.setOnCloseRequest {
+                        if (isDirty) {
+                            if (userConfirm("${document.title()} has not been saved. Save?", ButtonType.YES, ButtonType.CANCEL) == ButtonType.YES) {
+                                save()
+                                MainView.setAction("Closed ${document.title()}")
+                            } else {
+                                MainView.setAction("Not closing ${document.title()} ; It's not been saved.")
+                                it.consume()
+                            }
+                        }
+                    }
+                }
+
+                it.setOnClosed {
+                    openEditors.remove(second)
+                }
+            }
+        }
+    }
+
+    /**
+     * Loads a project document from disk and opens it.
+     *
+     * If there's already an editor, it's switched to.
+     * Else, one is created.
+     */
     fun openDocument(document: File) {
-        // TODO this loads the file from disk each time. Can we check to see if it's already loaded?
-        MainView.switchToDocument(Project.loadDocument(document))
+        switchToDocument(Project.loadDocument(document))
         MainView.setAction("Opened ${document.name}")
     }
+
+    /**
+     * This function switches to the editor tab that is passed in as a parameter
+     *
+     * @param editor DocumentEditor - The editor to switch to
+     */
+    fun switchToEditor(editor: DocumentEditor) {
+        MainView.dockEditors.selectionModel.select(editor.tab)
+        EventNotifier.notifyEvent(EventType.EDITOR_DOCUMENT_SWITCH)
+    }
+
+
+    /**
+     * This function switches to the editor tab that is passed in as a parameter
+     *
+     * @param editor DocumentEditor - The editor to switch to
+     */
+    fun DocumentEditor.Focus() = switchToEditor(this)
+
+
+    /**
+     * "If there's an editor for the given document, switch to it, otherwise create a new editor."
+     *
+     * The first line of the function is a call to the function findEditorFor, which returns an Editor?. If it's not null,
+     * the apply function is called on it. The apply function takes a lambda as its argument, and the lambda is executed
+     * with the Editor as its receiver. The lambda in this case is a call to the function switchToEditor, which takes an
+     * Editor as its argument
+     *
+     * @param document The document to switch to.
+     */
+    fun switchToDocument(document: Document) =
+        findEditorFor(document)?.apply { switchToEditor(this) }
+            ?: run { openDocument(document) }
+
+
+    //░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+    //endregion                                                     Editors
+    //░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+
+
+
 
     /**
      * Deletes [tag] after confirming with the user.
@@ -81,14 +214,6 @@ class MVC (
         MainView.setAction("Deleted tag(s)")
     }
 
-    private fun documentModified(document: Document) {
-        EventNotifier.notifyEvent(EventType.EDITOR_DOCUMENT_EDITED)
-        MainView.findEditorFor(document)?.apply {
-            dirty()
-            reRender()
-        }
-    }
-
     /**
      * When making many changes at once, this can be used to delay the
      * change notifications to after completing all changes.
@@ -109,7 +234,7 @@ class MVC (
         fun finishedModifying() {
             done = true
             distinct().forEach {
-                documentModified(it)
+                findEditorFor(it)!!.documentChanged()
             }
         }
 
